8ffc227ef2bef48affb3e9e7d8a1f3da5c02241c
sokkyyy <ndegwaraymond@gmail.com>
2020-02-21 17:17:14 +0300

    Final Weather API app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'http://api.openweathermap.org/data/2.5/weather?q=',
+  apiKey:'&appid=56825a2e75c4487be7e64a788b9952b4',
 };
+
